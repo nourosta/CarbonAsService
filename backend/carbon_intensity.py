a2f9ca526a6b10aca5814a72c3316bbf490ee055
nour osta <big.osta23@gmail.com>
2025-08-06 20:30:07 +0200

    Adding electricty maps carbon history

--- backend/carbon_intensity.py
+++ backend/carbon_intensity.py
@@ -15,7 +15,7 @@ def fetch_carbon_intensity(zone='FR', token='yqOVPpk1lZnhtkq1M4SK',temporal_gran
         raise Exception(f"API Error {response.status_code}: {response.text}")
 
 
-def fetch_history_carbon_intensity(zone='FR', token='yqOVPpk1lZnhtkq1M4SK', temporal_granularity='5_minutes'):
+def fetch_history_carbon_intensity(zone='FR', token='yqOVPpk1lZnhtkq1M4SK', temporal_granularity='15_minutes'):
     url = f"https://api.electricitymap.org/v3/carbon-intensity/latest?zone={zone}&temporalGranularity={temporal_granularity}"
     headers = {
         "auth-token": token
