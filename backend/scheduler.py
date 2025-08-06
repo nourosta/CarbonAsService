13f041defa162dc36aa4801572db639c41704ce7
nour osta <big.osta23@gmail.com>
2025-08-06 11:27:32 +0200

    Added energy mix scheduler

--- backend/scheduler.py
+++ backend/scheduler.py
@@ -1,5 +1,6 @@
-from crud import store_carbon_intensity
+from crud import store_carbon_intensity, store_power_breakdown
 from carbon_intensity import fetch_carbon_intensity
+from electricitymaps import fetch_power_breakdown
 from database import SessionLocal
 from apscheduler.schedulers.background import BackgroundScheduler
 
@@ -16,9 +17,22 @@ def scheduled_carbon_update():
     except Exception as e:
         print(f"❌ Error updating carbon intensity: {e}")
 
+
+def scheduled_electricitymix_update():
+    try:
+        zone = 'FR'
+        db = SessionLocal()
+        data = fetch_power_breakdown(zone)
+        store_power_breakdown(db, zone, data)
+        db.close()
+        print(f"✅ Power breakdown updated at {data.get('updatedAt')}")
+    except Exception as e:
+        print(f"❌ Error updating power breakdown: {e}")
+
 def start_scheduler():
     scheduler = BackgroundScheduler()
     scheduler.add_job(scheduled_carbon_update, 'interval', minutes=15)
+    scheduler.add_job(scheduled_electricitymix_update, 'interval', minutes=15)
     scheduler.start()
     print("🔄 Scheduler started: carbon intensity will update every 15 minutes.")
     return scheduler
